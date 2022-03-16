841c697b2ac248195b8a3c076100c21e3668c2f1
ParthParolekar <parolekarparth@gmail.com>
2022-03-16 19:14:33 +0530

    mockman added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import "./App.css";
+import Mockman from "mockman-js";
 
 import { Navbar } from "./components/";
 import { Authentication, Browse, Cart, Home, Wishlist } from "./pages";
 
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
         <Routes>
+          <Route path="/mock" element={<Mockman />} />
           <Route path="/" element={<Home />} />
           <Route path="/authentication" element={<Authentication />} />
           <Route path="/browse" element={<Browse />} />
