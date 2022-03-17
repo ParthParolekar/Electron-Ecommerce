87fc24d9d2bad8cc08c789cafb714860ef3051f9
ParthParolekar <parolekarparth@gmail.com>
2022-03-17 23:21:03 +0530

    feat: login functionality added with protected routes

--- src/App.js
+++ src/App.js
@@ -4,22 +4,69 @@ import Mockman from "mockman-js";
 import { Navbar } from "./components/";
 import { Authentication, Browse, Cart, Home, Wishlist } from "./pages";
 
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  useLocation,
+} from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useAuth } from "./Context/AuthContext/AuthContext";
 
 function App() {
+  const [userLoggedIn, setUserLoggedIn] = useState();
+  const [authState] = useAuth();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (authState.encodedToken === null) {
+      setUserLoggedIn(false);
+    } else {
+      setUserLoggedIn(true);
+    }
+  }, [authState]);
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/mock" element={<Mockman />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/authentication" element={<Authentication />} />
-          <Route path="/browse" element={<Browse />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/wishlist" element={<Wishlist />} />
-        </Routes>
-      </BrowserRouter>
+      {/* <BrowserRouter>  */}
+      <Navbar userLoggedIn={userLoggedIn} />
+      <Routes>
+        <Route path="/mock" element={<Mockman />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/authentication" element={<Authentication />} />
+        <Route path="/browse" element={<Browse />} />
+
+        <Route
+          path="/cart"
+          element={
+            userLoggedIn ? (
+              <Cart />
+            ) : (
+              <Navigate
+                to="/authentication"
+                state={{ from: location }}
+                replace
+              />
+            )
+          }
+        />
+        <Route
+          path="/wishlist"
+          element={
+            userLoggedIn ? (
+              <Wishlist />
+            ) : (
+              <Navigate
+                to="/authentication"
+                state={{ from: location }}
+                replace
+              />
+            )
+          }
+        />
+      </Routes>
+      {/* </BrowserRouter> */}
     </div>
   );
 }
---
+++ src/Context/AuthContext/AuthReducer.js
@@ -0,0 +1,52 @@
+export const initialState = {
+  foundUser: {
+    _id: null,
+    firstName: null,
+    lastName: null,
+    email: null,
+    cart: [],
+    wishlist: [],
+    id: null,
+  },
+  encodedToken: null,
+};
+
+export const authReducer = (
+  state,
+  { type, payload: { foundUser, encodedToken } }
+) => {
+  switch (type) {
+    case "LOGIN_USER":
+      return {
+        ...state,
+        foundUser: {
+          _id: foundUser._id,
+          firstName: foundUser.firstName,
+          lastName: foundUser.lastName,
+          email: foundUser.email,
+          cart: foundUser.cart,
+          wishlist: foundUser.wishlist,
+          id: foundUser.id,
+        },
+        encodedToken,
+      };
+
+    case "LOGOUT_USER":
+      return {
+        ...state,
+        foundUser: {
+          _id: foundUser._id,
+          firstName: foundUser.firstName,
+          lastName: foundUser.lastName,
+          email: foundUser.email,
+          cart: foundUser.cart,
+          wishlist: foundUser.wishlist,
+          id: foundUser.id,
+        },
+        encodedToken,
+      };
+
+    default:
+      return state;
+  }
+};
