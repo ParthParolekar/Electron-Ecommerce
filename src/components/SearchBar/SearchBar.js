4c5092f24c87cb59e3d6b7cf7320db478eeff916
Parth <parolekarparth@gmail.com>
2023-01-06 00:10:04 +0530

    search feature added

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { useFilter } from "../../Context/FilterContext/FilterContext";
 
 const SearchBar = () => {
+  const [filterState, filterDispatch] = useFilter();
+
+  const searchInputHandler = (e) => {
+    setTimeout(() => {
+      filterDispatch({ type: "FILTER_BY_SEARCH", payload: e.target.value });
+    }, 500);
+  };
   return (
     <div className="search flex-row align-center space-between">
-      <input type="text" placeholder="Looking for something?" />
+      <input
+        type="text"
+        placeholder="Looking for something?"
+        onChange={(e) => searchInputHandler(e)}
+      />
       <i className="fas fa-search"></i>
     </div>
   );
